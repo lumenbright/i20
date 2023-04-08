7be94c2ba7828aad9c587ed9ad1b4a0595e6ac05
lumenbright <lumen.0.bright@gmail.com>
2023-04-08 12:56:24 +0630

    aa

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
+let tickarr = [1,5,6,27,33,48,49,50,51,52];
+for(let start = 1; start <= 52; start++) {
+    document.querySelector('.page1').innerHTML += `
+        <div id="box${start}" class="abs"></div>
+    `;
+}
+for(let start = 53; start <= 59; start++) {
+    document.querySelector('.page2').innerHTML += `
+        <div id="box${start}" class="abs"></div>
+    `;
+}
+tickarr.map((item) => {
+    document.querySelector(`#box${item}`).classList.add('tick');
+});
+document.querySelector('#box59').style.fontSize = "1px";
+
 document.getElementById("btn").onclick = function() {
     
+    // gather input variables
     let name = document.getElementById("name").value;
     let email = document.getElementById("email").value;
     let emplid = document.getElementById("emplid").value;
@@ -19,29 +36,39 @@ document.getElementById("btn").onclick = function() {
     let emerrel = document.getElementById("emerrel").value;
     let emeremail = document.getElementById("emeremail").value;
     let emerphone = document.getElementById("emerphone").value;
+    let sponame = document.getElementById("sponame").value;
+    let sporelation = document.getElementById("sporelation").value;
+    let spoaddr = document.getElementById("spoaddr").value;
+    let spophone = document.getElementById("spophone").value;
 
 
-    let obj = {
-        name, email, emplid, gender, dob, phone, street, township, city, postcode, major, academicyear, semester, emername, emeremail, emerrel, emerphone
-    }
-
+    // covert functions
     let country = "Myanmar";
     let code = "+95";
+
     let namearr = name.split(" ");
     let firstname = namearr.slice(0, namearr.length - 1).join(" ");
     let lastname = namearr[namearr.length - 1];
+
+    let address = "No. (" + apartment + "), " + street + ", " + township + ", " + city + ", " + country;
+
     let male = gender ? "✓" : "";
     let female = gender ? "" : "✓";
-    let address = apartment + ", " + street + ", " + township + ", " + city + ", " + country;
-    phone = phone.split("").slice(1, phone.length).join("");
-    emerphone = emerphone.split("").slice(1, emerphone.length).join("");
     let yr23 = academicyear ? "O" : "";
     let yr24 = academicyear ? "" : "O";
     let semfall = semester ? "✓" : "";
     let semspring = semester ? "" : "✓";
+
+    phone = phone.split("").slice(1, phone.length).join("");
+    emerphone = emerphone.split("").slice(1, emerphone.length).join("");
+    spophone = spophone.split("").slice(1, spophone.length).join("");
+
     let dateofbirth = dob.split("-");
     dob = dateofbirth[1] + "/" + dateofbirth[2] + "/" + dateofbirth[0];
 
+
+    // put the values into placed divs
+    document.getElementById("box1").innerText = "✓";
     document.getElementById("box2").innerText = emplid;
     document.getElementById("box3").innerText = lastname;
     document.getElementById("box4").innerText = firstname;
@@ -93,8 +120,19 @@ document.getElementById("btn").onclick = function() {
     document.getElementById("box50").innerText = semfall;
     document.getElementById("box51").innerText = semspring;    
     document.getElementById("box52").innerText = "✓";
+    document.getElementById("box53").innerText = name;
+    document.getElementById("box54").innerText = sponame;
+    document.getElementById("box55").innerText = sporelation;
+    document.getElementById("box56").innerText = spoaddr;
+    document.getElementById("box57").innerText = country;
+    document.getElementById("box58").innerText = code + spophone;
+    document.getElementById("box59").innerText = name;
 
+
+    // hide the form and show the preview
     document.querySelector(".form").style.display = "none";
     document.querySelector(".preview").style.display = "block";
-}
 
+    alert("Please choose save as PDF");
+    window.print();
+}
